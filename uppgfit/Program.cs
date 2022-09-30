68a31056bd129673008e304c62bf6c6b1acdcccb
Alexsept177 <alan03002@utb.vaxjo.se>
2022-09-30 10:31:04 +0200

    Add project files.

---
+++ uppgfit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgfit
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int kr = timmar * 80;
+            
+
+            if (kr < 950)
+            {
+                Console.WriteLine("Det kommer kosta " + kr +  " kr för dig attt hyra bilen");   
+            }
+
+            else
+            {
+                Console.WriteLine("Du har nått max priset 950 kr");
+            }
+        }
+    }
+}
